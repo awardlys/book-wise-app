3eb74dd541028e8e7eceecd7ea8c73e9e71e0068
Rivaldo Mascarenhas <rivaldomascarenhas007@gmail.com>
2024-01-29 21:43:19 -0300

    refactor(*): import global.css

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
+import "./global.css";
 import { router } from "./router";
-import "./style.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
