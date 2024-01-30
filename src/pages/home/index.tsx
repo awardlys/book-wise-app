93b9337043a8070000fb88afc1d12959bcc892f2
Jonas <jonasexplore@gmail.com>
2024-01-29 21:58:20 -0300

    feat(home): starts sidebar menu component

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,7 +1,15 @@
 export function Home() {
   return (
-    <div>
-      <h1>Hello World</h1>
+    <div className="h-screen bg-gray-900 p-6">
+      <div className="bg-gray-800 w-[232px] rounded-md h-full flex flex-col justify-between items-center p-4">
+        <span className="font-bold text-lg text-purple-300">BookWise</span>
+        <ul className="list-none text-slate-400">
+          <li>Início</li>
+          <li>Explorar</li>
+          <li>Perfil</li>
+        </ul>
+        <div className="text-slate-400">Usuário</div>
+      </div>
     </div>
   );
 }
