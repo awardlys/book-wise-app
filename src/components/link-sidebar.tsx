e3320765af26c4aa41ab2c5cd1e9df1172a77751
rivaldo mascarenhas <rivaldomascarenhas007@gmail.com>
2024-02-11 23:13:23 -0300

    feat(components): create component link-sidebar

---
+++ src/components/link-sidebar.tsx
@@ -0,0 +1,27 @@
+import { ReactNode } from "react";
+import { Link, LinkProps } from "react-router-dom";
+
+interface LinkSidebarProps extends LinkProps {
+  current?: string;
+  name: string;
+  icon?: ReactNode;
+}
+export function LinkSidebar({
+  name,
+  current,
+  ...props
+}: Readonly<LinkSidebarProps>) {
+  return (
+    <Link {...props}>
+      <div className="flex gap-2 items-center">
+        <div
+          className={`${
+            current === props.to && "bg-gradient-to-br"
+          } from-green-300 to-violet-500 h-6 w-[2px] rounded-lg`}
+        />
+        {props.icon}
+        <h2 className="text-lg text-accent-foreground">{name}</h2>
+      </div>
+    </Link>
+  );
+}
--- src/components/sidebar.tsx
+++ src/components/sidebar.tsx
@@ -1,26 +1,34 @@
-import logo from "@/assets/logo-book.svg";
 import { GoGraph } from "react-icons/go";
 import { LuLogIn } from "react-icons/lu";
+import { useLocation } from "react-router-dom";
+import { LinkSidebar } from "./link-sidebar";
+import { Logo } from "./logo";
 import { Button } from "./ui/button";
 
 export function Sidebar() {
+  const { pathname } = useLocation();
   return (
-    <aside className="flex flex-col justify-between pt-10 pb-4 items-center dark:bg-gradient-to-tl rounded-lg dark:from-green-950 dark:via-gray-900 dark:to-purple-950 bg-gradient-to-tr from-green-500 via-gray-500 to-purple-700">
-      <div className="flex flex-col items-center justify-center gap-8">
-        <div className="flex gap-2">
-          <img src={logo} alt="Icone com um coração!" className="h-6 w-6" />
-          <span className="text-xl font-bold text-logo">BookWise</span>
-        </div>
-        <div className="flex items-center gap-2">
-          <div className="flex gap-2 items-center">
-            <div className="bg-gradient-to-br from-green-300 to-violet-500 h-6 w-[2px] rounded-lg" />
-            <GoGraph className="h-4 w-4 fill-accent-foreground" />
-            <h2 className="text-lg text-accent-foreground">Início</h2>
-          </div>
+    <aside className="flex flex-col justify-between h-[95vh] pt-10 pb-4 items-center dark:bg-gradient-to-tl rounded-lg dark:from-green-950 dark:via-gray-900 dark:to-purple-950 bg-gradient-to-tr from-green-500 via-gray-500 to-purple-700">
+      <div className="flex flex-col items-center justify-center gap-20">
+        <Logo variant={"home"} size={30} />
+        <div className="flex flex-col gap-6">
+          <LinkSidebar
+            to={"/home"}
+            name="Início"
+            current={pathname}
+            icon={<GoGraph className="h-4 w-4 fill-accent-foreground" />}
+          />
+          <LinkSidebar
+            to={"/explorer"}
+            name="Explorer"
+            current={pathname}
+            icon={<GoGraph className="h-4 w-4 fill-accent-foreground" />}
+          />
         </div>
       </div>
-      <Button  className="flex gap-2 items-center" variant={"ghost"} >
-       <LuLogIn/> Fazer login
+      <Button className="flex gap-2 items-center" variant={"ghost"}>
+        <LuLogIn />
+        <span> Fazer login</span>
       </Button>
     </aside>
   );
